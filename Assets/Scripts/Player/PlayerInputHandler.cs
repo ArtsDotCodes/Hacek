8a1cf96e69aca6a8a5df270d0dd2242369e0f188
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-10-10 19:33:12 -0400

    RailSwitchTest complete

---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputHandler : MonoBehaviour {
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PlayerManager.SetLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            PlayerManager.SetRight();
+        }
+	}
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour {
+
+    [SerializeField] private float lerpSpeed;
+
+    private GameObject currentRail;
+    private PlayerHitboxHandler leftHitbox, rightHitbox;
+    private static bool left, right;
+
+    void Start () {
+        currentRail = FindObjectOfType<RailHandler>().gameObject;
+        leftHitbox = GameObject.Find("LeftHitbox").GetComponent<PlayerHitboxHandler>();
+        rightHitbox = GameObject.Find("RightHitbox").GetComponent<PlayerHitboxHandler>();
+    }
+
+	void Update () {
+        if (left || right)
+            HandleRailSwitch();
+
+        SetAllFlagsFalse();
+	}
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentRail.transform.position + Vector3.up, Time.fixedDeltaTime * lerpSpeed);
+    }
+
+    private void HandleRailSwitch()
+    {
+        if (left)
+        {
+            List<GameObject> rails = leftHitbox.GetRails();
+            GameObject nextRail = null;
+            foreach (GameObject o in rails)
+            {
+                if(o != currentRail)
+                {
+                    if (nextRail == null)
+                        nextRail = o;
+                    else if (Vector3.Distance(transform.position, o.transform.position) < Vector3.Distance(transform.position, nextRail.transform.position))
+                        nextRail = o;
+                }   
+            }
+            if (nextRail != null)
+                currentRail = nextRail;
+        }
+        else if (right)
+        {
+            List<GameObject> rails = rightHitbox.GetRails();
+            GameObject nextRail = null;
+            foreach (GameObject o in rails)
+            {
+                if (o != currentRail)
+                {
+                    if (nextRail == null)
+                        nextRail = o;
+                    else if (Vector3.Distance(transform.position, o.transform.position) < Vector3.Distance(transform.position, nextRail.transform.position))
+                        nextRail = o;
+                }
+            }
+            if (nextRail != null)
+                currentRail = nextRail;
+        }
+    }
+
+    public static void SetLeft()
+    {
+        left = true;
+    }
+
+    public static void SetRight()
+    {
+        right = true;
+    }
+
+    private void SetAllFlagsFalse()
+    {
+        left = false;
+        right = false;
+    }
+}
