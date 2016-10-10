ee93066d9200bde006a8c5abe079cfb99c9e9a2d
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-10-10 01:26:04 -0400

    Added a test scene for trails

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    [SerializeField] private Vector3 rotations;
+    [SerializeField] private float speed;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(rotations * Time.fixedDeltaTime * speed);
+    }
+}
