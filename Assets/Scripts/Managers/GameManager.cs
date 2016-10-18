d171bb420a6b4a80d9931fad31bd860846efa8d0
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-10-18 02:15:43 -0400

    spawning in a circle

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
     public static string[,,] railData;
+    public static float timeScaleFactor = 1.0f;
 
     public static void SetRailData(string[,,] railData)
     {
--- Assets/Scripts/Managers/RailSpawnManager.cs
+++ Assets/Scripts/Managers/RailSpawnManager.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class RailSpawnManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private int numObjects = 1062;
+    private GameObject railMakerPrefab;
+
+    void Start()
+    {
+        print(ResourcePaths.TrailMakerPrefabPath);
+        railMakerPrefab = (GameObject)Resources.Load(ResourcePaths.TrailMakerPrefabPath);
+
+        print(railMakerPrefab);
+
+        Vector3 center = transform.position;
+        for (int i = 0; i < numObjects; i++)
+        {
+            Vector3 pos = RandomCircle(center, 50.0f);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+            Instantiate(railMakerPrefab, pos, rot);
+        }
+    }
+
+    private Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
 }
