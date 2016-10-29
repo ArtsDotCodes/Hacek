50fec6e19cc4b070a70d8ac3e6b7d6d58bd29c73
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2016-10-29 05:11:12 -0400

    vive controllers compatible

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class PlayerInputHandler : MonoBehaviour {
+
+    [SerializeField]
+    private SteamVR_TrackedController controller1, controller2;
+
+    void Start()
+    {
+        controller1.PadClicked += HandlePadClick;
+        controller2.PadClicked += HandlePadClick;
+    }
 	
 	void Update () {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
@@ -13,4 +22,12 @@ public class PlayerInputHandler : MonoBehaviour {
             PlayerManager.SetRight();
         }
 	}
+
+    private void HandlePadClick(object sender, ClickedEventArgs e)
+    {
+        if(e.padX < 0.0f)
+            PlayerManager.SetLeft();
+        else
+            PlayerManager.SetRight();
+    }
 }
